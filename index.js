450d5e5c8f15910abbe52d160eace152880ed23b
Hack Reactor Student <student@hackreactor.com>
2017-05-02 20:43:44 -0700

    JT Socks

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('JT ROCKS!!!');
+  res.send('JT SOCKS!!!');
 });
 
 var port = process.env.PORT || 3000;
