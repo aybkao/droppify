0cb4d0ce185ed313e7690590283daff5275129e0
Max Braz <braz.max@gmail.com>
2017-05-09 18:25:47 -0700

    merge conflicts

--- react-client/src/components/Input.jsx
+++ react-client/src/components/Input.jsx
@@ -64,8 +64,7 @@ class Input extends React.Component {
         <div>
           {this.state.uploadedFileCloudinaryUrl === '' ? null :
           <div>
-            <p>{this.state.uploadedFile.name}</p>
-            <img src={this.state.uploadedFileCloudinaryUrl} />
+            <p> Thank you, your file is being uploaded </p>
           </div>}
         </div>
       </form>
@@ -74,78 +73,3 @@ class Input extends React.Component {
 }
 
 export default Input;
-//     this.state = {
-//       uploadedFileCloudinaryUrl: '',
-//       uploadedFile: []
-//     }
-//     this.onImageDrop = this.onImageDrop.bind(this);
-//     this.handleImageUpload = this.handleImageUpload.bind(this);
-//   }
-  
-//   onImageDrop(files) {
-//     this.setState({
-//       uploadedFile: files[0]
-//     });
-//     console.log('this is the files[0]: ', files[0]);
-
-//     this.handleImageUpload(files[0]);
-//   } 
-
-//   handleImageUpload(file) {
-//     $.cloudinary.config({cloud_name: config.cloud_name, api_key: config.api_key, api_secret: config.api_secret})
-//     $(() => {
-//       if ($.fn.cloudinary_fileupload !== undefined) {
-//         console.log('this is the file: ', file);
-//         $(`input.cloudinary-fileupload[type=${file}]`).cloudinary_fileupload();
-//       } else {
-//         console.log('there was an error');
-//       }
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <Dropzone
-//         multiple={false}
-//         onDrop={this.onImageDrop}
-//         // name="file" 
-//         // type="file" 
-//         // className="cloudinary-fileupload"
-//         // data-cloudinary-field="image_id" 
-//         // data-form-data=" ... html-escaped JSON data ... "
-//       >
-//         <p>Drop an image or click to select a file to upload.</p>
-//       </Dropzone>
-//     );
-//   }
-// }
-
-// // Input example directly from cloudinary
-//       // <input name="file" type="file" 
-//       //   className="cloudinary-fileupload" data-cloudinary-field="image_id" 
-//       //   data-form-data=" ... html-escaped JSON data ... " 
-//       //   onChange={this.onImageDrop}
-//       // />
-
-
-// //ajax call with superagent
-//     // let upload = request.post(CLOUDINARY_UPLOAD_URL)
-//     //   .field('headers', 'Access-Control-Allow-Origin')
-//     //   .field('upload-preset', CLOUDINARY_UPLOAD_PRESET)
-//     //   .field('cloud_name', config.cloud_name)
-//     //   .field('api_key', config.api_key)
-//     //   .field('file', file);
-    
-//     // upload.end((err, res) => {
-//     //   if (err) {
-//     //     console.error(err);
-//     //     console.log('************** there was an error   *************')
-//     //   } 
-
-//     //   if (res.body.secure_url !== '') {
-//     //     this.setState({
-//     //       uploadedFileCloudinaryUrl: res.body.secure_url
-//     //     });
-//     //       console.log('image uploaded to cloudinary!')
-//     //   }
-//     // });
