1a06c32b685b921bfe9271c3911ee66898f84cb2
Max Braz <braz.max@gmail.com>
2017-05-11 14:51:45 -0700

    add page number component to render here instead of index.jsx

--- react-client/src/components/TableView.jsx
+++ react-client/src/components/TableView.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Nav from './Nav.jsx';
+import PageNumber from './PageNumber.jsx';
+
 import {
   Table,
   TableBody,
@@ -9,23 +12,27 @@ import {
 } from 'material-ui/Table';
 // TO DO:
 const TableView = (props) => (
-  <Table onRowSelection={props.handleRowSelection} >
-    <TableHeader >
-      <TableRow >
-      { Object.keys(props.items[0]).map( (key)=> ( 
-          <TableHeaderColumn key={key} style={{ whiteSpace:false, wordWrap:'break-word'}}>{key}</TableHeaderColumn>
+  <div>
+    <Nav />
+    <Table onRowSelection={props.handleRowSelection} >
+      <TableHeader >
+        <TableRow >
+        { Object.keys(props.items[0]).map( (key)=> ( 
+            <TableHeaderColumn key={key} style={{ whiteSpace:false, wordWrap:'break-word'}}>{key}</TableHeaderColumn>
+          ) )}
+        </TableRow>
+      </TableHeader>
+      <TableBody >
+        { props.items.slice(1, props.items.length).map( (val, key1)=>(
+          <TableRow selected={props.isSelected(key1)}>
+          {Object.keys(val).map( (key2)=>(
+            <TableRowColumn key={key2} style={{ whiteSpace:false, wordWrap:'break-word'}}>{val[key2]}</TableRowColumn>
+          ) )}
+          </TableRow> 
         ) )}
-      </TableRow>
-    </TableHeader>
-    <TableBody >
-      { props.items.slice(1, props.items.length).map( (val, key1)=>(
-        <TableRow selected={props.isSelected(key1)}>
-        {Object.keys(val).map( (key2)=>(
-          <TableRowColumn key={key2} style={{ whiteSpace:false, wordWrap:'break-word'}}>{val[key2]}</TableRowColumn>
-        ) )}
-        </TableRow> 
-      ) )}
-    </TableBody>
-  </Table>
+      </TableBody>
+    </Table>
+    <PageNumber />
+  </div>
 )
 export default TableView;
