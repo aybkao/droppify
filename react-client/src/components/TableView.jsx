db4c600d7f53f7f6f5a106c6171b6a5eb271a0c4
Max Braz <braz.max@gmail.com>
2017-05-12 00:22:12 -0700

    merge

--- react-client/src/components/TableView.jsx
+++ react-client/src/components/TableView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './Nav.jsx';
 import PageNumber from './PageNumber.jsx';
-
+import List from './List.jsx';
 import {
   Table,
   TableBody,
@@ -10,21 +10,37 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
-// TO DO:
+
 const TableView = (props) => (
+
+import 
+  {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+  } 
+from 'material-ui/Table';
+
+const TableView = ({items, isSelected, handleRowSelection}) => (
   <div>
     <Nav />
-    <Table onRowSelection={props.handleRowSelection} >
-      <TableHeader >
-        <TableRow >
-        { Object.keys(props.items[0]).map( (key)=> ( 
+      <List items={items} />
+
+    <Table onRowSelection={handleRowSelection} >
+      <TableHeader displaySelectAll={false} adjustForCheckbox={false} >
+        <TableRow>
+        { Object.keys(items[0]).map( (key)=> ( 
             <TableHeaderColumn key={key} style={{ whiteSpace:false, wordWrap:'break-word'}}>{key}</TableHeaderColumn>
-          ) )}
+          )
+        )}
         </TableRow>
       </TableHeader>
-      <TableBody >
-        { props.items.slice(1, props.items.length).map( (val, key1)=>(
-          <TableRow selected={props.isSelected(key1)}>
+      <TableBody displayRowCheckbox={false}>
+        { items.map( (val, key1)=>(
+          <TableRow selected={isSelected(key1)}>
           {Object.keys(val).map( (key2)=>(
             <TableRowColumn key={key2} style={{ whiteSpace:false, wordWrap:'break-word'}}>{val[key2]}</TableRowColumn>
           ) )}
@@ -32,7 +48,6 @@ const TableView = (props) => (
         ) )}
       </TableBody>
     </Table>
-    <PageNumber />
   </div>
 )
 export default TableView;
