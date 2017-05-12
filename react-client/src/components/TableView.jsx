779a268c526d6c2382b36f83084cb2165fe8b0cc
Max Braz <braz.max@gmail.com>
2017-05-11 23:14:42 -0700

    merge conficts

--- react-client/src/components/TableView.jsx
+++ react-client/src/components/TableView.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Nav from './Nav.jsx';
+import PageNumber from './PageNumber.jsx';
+
 import {
   Table,
   TableBody,
@@ -27,5 +30,28 @@ const TableView = (props) => (
       ) )}
     </TableBody>
   </Table>
+
+  <div>
+    <Nav />
+    <Table onRowSelection={props.handleRowSelection} >
+      <TableHeader >
+        <TableRow >
+        { Object.keys(props.items[0]).map( (key)=> ( 
+            <TableHeaderColumn key={key} style={{ whiteSpace:false, wordWrap:'break-word'}}>{key}</TableHeaderColumn>
+          ) )}
+        </TableRow>
+      </TableHeader>
+      <TableBody >
+        { props.items.slice(1, props.items.length).map( (val, key1)=>(
+          <TableRow selected={props.isSelected(key1)}>
+          {Object.keys(val).map( (key2)=>(
+            <TableRowColumn key={key2} style={{ whiteSpace:false, wordWrap:'break-word'}}>{val[key2]}</TableRowColumn>
+          ) )}
+          </TableRow> 
+        ) )}
+      </TableBody>
+    </Table>
+    <PageNumber />
+  </div>
 )
 export default TableView;
