c952ab2380384177919dedf46d0e40b5f232d4ad
Max Braz <braz.max@gmail.com>
2017-05-14 13:34:46 -0700

    import functions from db, multer add post to receive file

--- server/index.js
+++ server/index.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const pdf_table_extractor = require("pdf-table-extractor"); //<-- FF
+const pdfTableExtractor = require('pdf-table-extractor');
 const allSchemas = require('../database-mongo/dbPull.js');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const configAuth = require('./configAuth.js');
+const tableParse = require('../database-mongo/index.js');
+const multer = require('multer');
+var upload = multer({ dest: '../PDF'});
 
 var User = allSchemas.user;
 var Items = allSchemas.items;
 // const post = require('../database-mongo/index.js');
 const app = express();
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../public')));
 
 ////////////GOOGLE STRATEGY///////////////////
 passport.use(new GoogleStrategy({
-    clientID: configAuth.googleAuth.clientID,
-    clientSecret: configAuth.googleAuth.clientSecret,
-    callbackURL: configAuth.googleAuth.callbackURL
-  },
+  clientID: configAuth.googleAuth.clientID,
+  clientSecret: configAuth.googleAuth.clientSecret,
+  callbackURL: configAuth.googleAuth.callbackURL
+},
   function(accessToken, refreshToken, profile, done) {
     console.log('accessToken', accessToken);
     console.log('refreshToken', refreshToken);
@@ -63,7 +68,7 @@ app.get('/auth/google', passport.authenticate('google',
 
 app.get('/auth/google/callback', 
   passport.authenticate('google', {failureRedirect: '/'}), function(req, res) {
-    res.redirect('/profile')
+    res.redirect('/profile');
   });
 
 app.get('/profile', function (req, res) {
@@ -72,60 +77,61 @@ app.get('/profile', function (req, res) {
 });
 ////////////END OF GOOGLE STRATEGY////////////
 
-
-
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/../public')));
-
 // :kw takes in /items/<any keyword> and passes the kw to req.params.kw below. 
 app.get('/items/:kw', function (req, res) {  
   var arr = [];
   var filterObj = [];
   var filterArr = [];
   // Goal here is to create a full search on all columns in the table
-    Items.findOne( function(err, val) {
+  Items.findOne(function (err, val) {
     val = JSON.parse(JSON.stringify(val));
-    Object.keys(val).map( (key, index) =>  { arr.push(key) })
+    Object.keys(val).map((key, index) => { arr.push(key); });
     // val is an Array
     // Loop is used to build an object with regex values.
-    for (var i = 0; i < arr.length-1; i++){
+    for (var i = 0; i < arr.length - 1; i++) {
       var key = arr[i];
-      filterObj[arr[i]]={"$regex": req.params.kw, "$options":"i"}
+      filterObj[arr[i]] = { '$regex': req.params.kw, '$options': 'i' };
     }
     // loop here is used to place data into the $or find method.
     for (var key in filterObj) {
       var smallObj = {};
-      smallObj[key] = filterObj[key]
-      smallObj = JSON.parse(JSON.stringify(smallObj))
-      filterArr.push( smallObj )
+      smallObj[key] = filterObj[key];
+      smallObj = JSON.parse(JSON.stringify(smallObj));
+      filterArr.push(smallObj);
     }
     // removing database added columns _id and __v from search
-    filterArr = filterArr.slice(1,filterArr.length)
+    filterArr = filterArr.slice(1, filterArr.length);
     var bigObj = {};
-    bigObj.$or = filterArr
-    console.log('DOES IS REACH?', bigObj)
+    bigObj.$or = filterArr;
+    console.log('DOES IS REACH?', bigObj);
     // bigObj is the main algorithm used to find data in the table    
     Items.find( bigObj ).limit(100).exec( function (err, data) {
       if (err) {
-        console.log( 'server get request failure', err)
+        console.log( 'server get request failure', err);
       } else {
-        console.log('server get request Success!')
+        console.log('server get request Success!');
       }
       res.end(JSON.stringify(data));
-    })  
-  })
+    });
+  });
 });
 
 app.post('/data', function(req, res) {
 	// take req.body.filter to pull data from our database
   // cloudinary.api.resources(function(result){console.log('RESULT',result)})
-	console.log('Fake Post Data')
-  res.send('yoiasf')
-})
+  console.log('Fake Post Data');
+  res.send('yoiasf');
+});
 
 app.post('/url', (req, res) => {
-  console.log('************** ', req.body);
-	res.send('received data');
+  res.send('received url and title');
+});
+
+app.post('/upload', upload.single('file'), (req, res, next) => {
+  console.log(req.file);
+  
+  pdfTableExtractor(req.file, tableParse.tableParseSuccess, tableParse.tableParseError);
+  res.end(req.file);
 });
 
 app.get('/allItems', function (req, res) {
@@ -133,12 +139,12 @@ app.get('/allItems', function (req, res) {
   // Items.find({"Course Location ": "San Jose " }).limit(30).exec( function (err, data) {
   Items.find({}).limit(100).exec( function (err, data) {
     if (err) {
-      console.log( 'server get request failure', err)
+      console.log( 'server get request failure', err);
     } else {
-      console.log('Success!')
+      console.log('Success!');
     }
     res.end(JSON.stringify(data));
-  })
+  });
 });
 
 const port = process.env.PORT || 5000;
