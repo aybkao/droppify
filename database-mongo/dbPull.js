a74a0a4bcaeaae237a8181e14a6dcc126325a98e
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-05-14 21:01:07 -0700

    Working on Import

--- database-mongo/dbPull.js
+++ database-mongo/dbPull.js
@@ -3,7 +3,7 @@
 
 //################################################
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/test11');
+// mongoose.connect('mongodb://localhost/test14');
 mongoose.connect('mongodb://freedomfighters:freedomfighters@ds133231.mlab.com:33231/heroku_tn5ml1b6');
 
 var db = mongoose.connection;
--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 let filePath;
-// mongoose.connect('mongodb://localhost/test11');
-mongoose.connect('mongodb://freedomfighters:freedomfighters@ds133231.mlab.com:33231/heroku_tn5ml1b6');
+// mongoose.createConnection('mongodb://localhost/test14');
+mongoose.createConnection('mongodb://freedomfighters:freedomfighters@ds133231.mlab.com:33231/heroku_tn5ml1b6');
 var db = mongoose.connection;
 
 db.on('error', function() {
