e269aec6af1b33bca6ac8ac8f15916faae891d93
Max Braz <braz.max@gmail.com>
2017-05-15 15:19:38 -0700

    working on searchbar componnent

--- react-client/src/components/SearchBar.jsx
+++ react-client/src/components/SearchBar.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Nav from './components/Nav.jsx';
 
 const Search = ({handleChange, handleClick}) => (
+
   <div>
     <input type='text' value='value' onChange={handleChange.bind(this, )}/>
     <input type='button' value='Submit' onClick={handleClick}/>
